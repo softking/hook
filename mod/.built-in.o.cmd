160a8ef0043f560b84d81dacbce7ff8cacc4db3a
空巷 <shaolei.ssl@alipay.com>
2019-02-23 14:58:18 +0800

    init

--- mod/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/sunshaolei/kmod/built-in.o :=  ld -m elf_x86_64   -r -o /root/sunshaolei/kmod/built-in.o /root/sunshaolei/kmod/libudis86/built-in.o ; scripts/mod/modpost /root/sunshaolei/kmod/built-in.o
